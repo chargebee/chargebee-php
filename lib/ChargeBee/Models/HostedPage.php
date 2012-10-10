fdf65f3ebd391e010e09c161521222a37c4982cc
Thiyagu <thiyagu@bubblepath.com>
2012-10-10 20:03:21 +0530

    HostedPage related changes

--- lib/ChargeBee/Models/HostedPage.php
+++ lib/ChargeBee/Models/HostedPage.php
@@ -29,6 +29,16 @@ class ChargeBee_HostedPage extends ChargeBee_Model
     return ChargeBee_Request::send(ChargeBee_Request::POST, "/hosted_pages/update_card", $params, $env);
   }
 
+  public static function checkoutOnetimeCharge($params, $env = null)
+  {
+    return ChargeBee_Request::send(ChargeBee_Request::POST, "/hosted_pages/checkout_onetime_charge", $params, $env);
+  }
+
+  public static function checkoutOnetimeAddons($params, $env = null)
+  {
+    return ChargeBee_Request::send(ChargeBee_Request::POST, "/hosted_pages/checkout_onetime_addons", $params, $env);
+  }
+
   public static function retrieve($id, $env = null)
   {
     return ChargeBee_Request::send(ChargeBee_Request::GET, "/hosted_pages/$id", array(), $env);
