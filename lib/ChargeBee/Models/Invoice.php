4d3bff824d63a5637dd77bfa4eea16e8d85be742
Thiyagu <thiyagu@bubblepath.com>
2013-07-31 13:43:54 +0530

    minor code re-org

--- lib/ChargeBee/Models/Invoice.php
+++ lib/ChargeBee/Models/Invoice.php
@@ -11,14 +11,14 @@ class ChargeBee_Invoice extends ChargeBee_Model
   # OPERATIONS
   #-----------
 
-  public static function addCharge($id, $params, $env = null)
+  public static function charge($params, $env = null)
   {
-    return ChargeBee_Request::send(ChargeBee_Request::POST, "/invoices/$id/add_charge", $params, $env);
+    return ChargeBee_Request::send(ChargeBee_Request::POST, "/invoices/charge", $params, $env);
   }
 
-  public static function addAddonCharge($id, $params, $env = null)
+  public static function chargeAddon($params, $env = null)
   {
-    return ChargeBee_Request::send(ChargeBee_Request::POST, "/invoices/$id/add_addon_charge", $params, $env);
+    return ChargeBee_Request::send(ChargeBee_Request::POST, "/invoices/charge_addon", $params, $env);
   }
 
   public static function all($params = array(), $env = null)
@@ -36,19 +36,19 @@ class ChargeBee_Invoice extends ChargeBee_Model
     return ChargeBee_Request::send(ChargeBee_Request::GET, "/invoices/$id", array(), $env);
   }
 
+  public static function addCharge($id, $params, $env = null)
+  {
+    return ChargeBee_Request::send(ChargeBee_Request::POST, "/invoices/$id/add_charge", $params, $env);
+  }
+
+  public static function addAddonCharge($id, $params, $env = null)
+  {
+    return ChargeBee_Request::send(ChargeBee_Request::POST, "/invoices/$id/add_addon_charge", $params, $env);
+  }
+
   public static function collect($id, $env = null)
   {
     return ChargeBee_Request::send(ChargeBee_Request::POST, "/invoices/$id/collect", array(), $env);
-  }
-
-  public static function charge($params, $env = null)
-  {
-    return ChargeBee_Request::send(ChargeBee_Request::POST, "/invoices/charge", $params, $env);
-  }
-
-  public static function chargeAddon($params, $env = null)
-  {
-    return ChargeBee_Request::send(ChargeBee_Request::POST, "/invoices/charge_addon", $params, $env);
   }
 
  }
