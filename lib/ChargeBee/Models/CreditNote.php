7d5946a37112e466728f894a86a0f471e2f29527
SangeethaBaskaran <sangeetha@chargebee.com>
2016-08-02 16:06:47 +0530

    * * * ** APIs added**: A new endpoint to retrieve Credit Note as PDF. See : https://apidocs.chargebee.com/docs/api/credit_notes#retrieve_credit_note_as_pdf
    ** APIs updated**:
    The attribute "invoice notes" is added to subscription in Hosted Pages Checkout New and Checkout Existing API's.
    See : https://apidocs.chargebee.com/docs/api/hosted_pages#checkout_new_subscription
    
    The filter parameter "paid_at" is added to list invoices and "paid_on_after" is deprecated. See : https://apidocs.chargebee.com/docs/api/invoices#list_invoices
    
    The filter parameter "occurred_at", "webhook_status", "event_type" is added to list events and parameter "start_time", "end_time", "webhook_status", "event_type" is deprecated. See : https://apidocs.chargebee.com/docs/api/events#list_events

--- lib/ChargeBee/Models/CreditNote.php
+++ lib/ChargeBee/Models/CreditNote.php
@@ -16,6 +16,11 @@ class ChargeBee_CreditNote extends ChargeBee_Model
     return ChargeBee_Request::send(ChargeBee_Request::GET, ChargeBee_Util::encodeURIPath("credit_notes",$id), array(), $env, $headers);
   }
 
+  public static function pdf($id, $env = null, $headers = array())
+  {
+    return ChargeBee_Request::send(ChargeBee_Request::POST, ChargeBee_Util::encodeURIPath("credit_notes",$id,"pdf"), array(), $env, $headers);
+  }
+
   public static function all($params = array(), $env = null, $headers = array())
   {
     return ChargeBee_Request::sendListRequest(ChargeBee_Request::GET, ChargeBee_Util::encodeURIPath("credit_notes"), $params, $env, $headers);
